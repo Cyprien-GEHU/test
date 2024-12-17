64b976a1eeb5c28daa7ecf1d5666b31b4cb33390
Cyprien <cypriengehu@example.com>
2024-12-17 15:11:28 +0000

    test 3

--- main.c
+++ main.c
@@ -6,7 +6,36 @@
  *
  */
 
-int main(int av, char **arc, char **env)
+int main(int av, char **arc)
 {
-	
+	char *be, *splity;
+	size_t n;
+	int test;
+	av = 0;
+
+	printf("$ ");
+	test = getline(&be, &n, stdin);
+	if (test == -1)
+	{
+		printf("error");
+		return(0);
+	}
+
+	be[strcspn(be, "\n")] = 0;
+	splity = strtok(be, " ");
+
+	while (splity)
+	{
+		arc[av++] = splity;
+		splity = strtok(NULL, " ");
+	}
+
+	if (execve("/bin/ls", arc, NULL) == -1)
+	{
+		perror("Error executing command");
+		free(be);
+		return (1);
+	}
+
+	return(0);	
 }
